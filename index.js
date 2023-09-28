6f6499bb00de61d71e68b895e0784c67652458ba
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2023-09-28 17:12:47 +0500

    all fields setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { PKPass } = require("passkit-generator");
 const fs = require("fs");
 const path = require("path");
+const axios = require('axios');
+
 
 const wwdr = fs.readFileSync(path.resolve(__dirname, "./wwdr.pem"));
 const signerCert = fs.readFileSync(path.resolve(__dirname, "./signerCert.pem"));
@@ -10,17 +12,17 @@ const signerKey = fs.readFileSync(path.resolve(__dirname, "./signerKey.pem"));
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({"test":'Hello World!'});
 });
 
-app.get('/pass', async (req, res) => {
+app.post('/pass', async (req, res) => {
     try{
+        const { primary_fields, secondary_fields, auxiliary_fields, thumbnail, logo, card_url  } = req.body;
+        console.log(req.body);
         const pass = await PKPass.from({
-            /**
-             * Note: .pass extension is enforced when reading a
-             * model from FS, even if not specified here below
-             */
             model: "./model/DBC.pass",
             certificates: {
                 wwdr,
@@ -37,33 +39,42 @@ app.get('/pass', async (req, res) => {
             description: "Demo pass",
         });
 
-        pass.primaryFields.push({
-            key: "name",
-            label: "Name",
-            value: "Jon Doe",
-        })
+        pass.headerFields.push(primary_fields);
+        // pass.primaryFields.push(primary_fields)
 
-        pass.secondaryFields.push({
-            key: "bank",
-            label: "Bank",
-            value: "UBL",
-        })
+        pass.secondaryFields.push(secondary_fields)
 
-        pass.auxiliaryFields.push({
-            key: "balance",
-            label: "Balance",
-            value: "$10",
-        })
+        pass.auxiliaryFields.push(auxiliary_fields)
+
+        pass.setBarcodes({
+            message: card_url,
+            format: "PKBarcodeFormatQR",
+            altText: "Scan to visit card"
+        });
+
+        console.log(Date.now());
+
+        const thumbnailRes = await axios.get(thumbnail, {responseType: 'arraybuffer'})
+        const thumbnailBuffer = Buffer.from(thumbnailRes.data, 'utf-8');
+
+        const logoRes = await axios.get(logo, {responseType: 'arraybuffer'})
+        const logoBuffer = Buffer.from(logoRes.data, 'utf-8');
+
+        pass.addBuffer("thumbnail.png", thumbnailBuffer);
+        pass.addBuffer("logo.png", logoBuffer);
 
         const buffer = pass.getAsBuffer();
+
+        const passName = Date.now() * 1000 * Math.floor(Math.random() * 9999);
+
         // storage to local file
-        // fs.writeFileSync("myFirstPass.pkpass", buffer);
+        fs.writeFileSync(`${passName}.pkpass`, buffer);
         console.log("Pass created successfully");
 
-        res.setHeader('Content-Type', 'application/vnd.apple.pkpass');
-        res.setHeader('Content-Disposition', 'attachment; filename=pass.pkpass');
-        res.send(buffer);
-        // res.json({"msg":'created.'})
+        // res.setHeader('Content-Type', 'application/vnd.apple.pkpass');
+        // res.setHeader('Content-Disposition', 'attachment; filename=pass.pkpass');
+        // res.send(buffer);
+        res.json({"msg":'created.'})
     }
     catch (error) {
         console.error('Error creating pass:', error);
